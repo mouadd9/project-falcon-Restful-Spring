4906b4e1e6a6893f8017c064721ddfb41347281f
moad oualla <moad.oualla@gmail.com>
2025-05-07 17:59:31 +0100

    ec2 and openvpn integrated

---
+++ src/main/java/com/falcon/falcon/dto/cloudDto/InstanceActionRequest.java
@@ -0,0 +1,13 @@
+package com.falcon.falcon.dto.cloudDto;
+
+public class InstanceActionRequest {
+    private String instanceId;
+
+    public String getInstanceId() {
+        return instanceId;
+    }
+
+    public void setInstanceId(String instanceId) {
+        this.instanceId = instanceId;
+    }
+}
---
+++ src/main/java/com/falcon/falcon/dto/cloudDto/InstanceActionResponse.java
@@ -0,0 +1,49 @@
+package com.falcon.falcon.dto.cloudDto;
+
+public class InstanceActionResponse {
+    private String instanceId;
+    private boolean success;
+    private String message;
+    private String status;
+    private String privateIpAddress;
+
+    public String getInstanceId() {
+        return instanceId;
+    }
+
+    public void setInstanceId(String instanceId) {
+        this.instanceId = instanceId;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getPrivateIpAddress() {
+        return privateIpAddress;
+    }
+
+    public void setPrivateIpAddress(String privateIpAddress) {
+        this.privateIpAddress = privateIpAddress;
+    }
+}
