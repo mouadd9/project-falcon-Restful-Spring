a4bbab08203cba149fb4cf724f273f06d13f9f2c
mouadispunk <bencaidmouad9@gmail.com>
2025-05-21 21:38:50 +0100

    added controllers

---
+++ src/main/java/com/falcon/falcon/controllers/EC2Controller.java
@@ -0,0 +1,74 @@
+package com.falcon.falcon.controller;
+
+import com.falcon.falcon.dto.cloudDto.CreateInstanceResponse;
+import com.falcon.falcon.dto.cloudDto.InstanceActionRequest;
+import com.falcon.falcon.dto.cloudDto.InstanceActionResponse;
+import com.falcon.falcon.service.impl.Ec2Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.CompletableFuture;
+
+@RestController
+@RequestMapping("/ec2")
+public class EC2Controller {
+
+    private static final Logger logger = LoggerFactory.getLogger(EC2Controller.class);
+
+    @Autowired
+    private Ec2Service ec2Service;
+
+    @PostMapping("/create")
+    public CompletableFuture<CreateInstanceResponse> createInstance(@RequestParam String amiId) {
+        logger.info("Received request to create instance with AMI ID: {}", amiId);
+        return ec2Service.createInstance(amiId)
+                .whenComplete((response, throwable) -> {
+                    if (throwable != null) {
+                        logger.error("Error in create instance response: {}", throwable.getMessage());
+                    } else {
+                        logger.info("Successfully created instance: {}", response.getInstanceId());
+                    }
+                });
+    }
+
+    @PostMapping("/stop")
+    public CompletableFuture<InstanceActionResponse> stopInstance(@RequestBody InstanceActionRequest request) {
+        logger.info("Received request to stop instance: {}", request.getInstanceId());
+        return ec2Service.stopInstance(request.getInstanceId())
+                .whenComplete((response, throwable) -> {
+                    if (throwable != null) {
+                        logger.error("Error stopping instance: {}", throwable.getMessage());
+                    } else {
+                        logger.info("Stop instance response: {}", response.getMessage());
+                    }
+                });
+    }
+
+    @PostMapping("/start")
+    public CompletableFuture<InstanceActionResponse> startInstance(@RequestBody InstanceActionRequest request) {
+        logger.info("Received request to start instance: {}", request.getInstanceId());
+        return ec2Service.startInstance(request.getInstanceId())
+                .whenComplete((response, throwable) -> {
+                    if (throwable != null) {
+                        logger.error("Error starting instance: {}", throwable.getMessage());
+                    } else {
+                        logger.info("Start instance response: {}", response.getMessage());
+                    }
+                });
+    }
+
+    @PostMapping("/terminate")
+    public CompletableFuture<InstanceActionResponse> terminateInstance(@RequestBody InstanceActionRequest request) {
+        logger.info("Received request to terminate instance: {}", request.getInstanceId());
+        return ec2Service.terminateInstance(request.getInstanceId())
+                .whenComplete((response, throwable) -> {
+                    if (throwable != null) {
+                        logger.error("Error terminating instance: {}", throwable.getMessage());
+                    } else {
+                        logger.info("Terminate instance response: {}", response.getMessage());
+                    }
+                });
+    }
+}
---
+++ src/main/java/com/falcon/falcon/controllers/OpenVPNController.java
@@ -0,0 +1,38 @@
+package com.falcon.falcon.controller;
+
+import com.falcon.falcon.service.impl.OpenVPNServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/vpn")
+public class OpenVPNController {
+
+    private static final Logger logger = LoggerFactory.getLogger(OpenVPNController.class);
+    private final OpenVPNServiceImpl openVPNService;
+
+    // Explicit constructor for dependency injection
+    public OpenVPNController(OpenVPNServiceImpl openVPNService) {
+        this.openVPNService = openVPNService;
+    }
+
+    @GetMapping("/users/{username}/config")
+    public ResponseEntity<byte[]> getUserConfig(@PathVariable String username) {
+        try {
+            byte[] configFile = openVPNService.generateUserConfig(username);
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+            headers.setContentDispositionFormData("attachment", username + ".ovpn");
+
+            return new ResponseEntity<>(configFile, headers, HttpStatus.OK);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
+    }
+}
