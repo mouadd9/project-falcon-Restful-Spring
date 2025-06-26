defa99915fbc1cd43756386ffbe89b51761293af
mouadispunk <bencaidmouad9@gmail.com>
2025-06-26 22:40:49 +0100

    added initial setup for the config server

---
+++ instance-service/src/main/java/org/falcon/instanceservice/InstanceServiceApplication.java
@@ -0,0 +1,13 @@
+package org.falcon.instanceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstanceServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InstanceServiceApplication.class, args);
+    }
+
+}
---
+++ instance-service/src/test/java/org/falcon/instanceservice/InstanceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.falcon.instanceservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstanceServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
