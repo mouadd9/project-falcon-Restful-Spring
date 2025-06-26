9783a2f26a6956a6bd99921e7baebbec5fcf2e76
mouadispunk <bencaidmouad9@gmail.com>
2025-06-26 20:24:06 +0100

    refactored the Spring Cloud Gateway configuration and dependencies

--- progression-service/src/main/java/org/falcon/progressionservice/configuration/FeignExceptionConfig.java
+++ progression-service/src/main/java/org/falcon/progressionservice/configuration/FeignExceptionConfig.java
@@ -3,7 +3,9 @@ package org.falcon.progressionservice.configuration;
 import feign.codec.ErrorDecoder;
 import org.falcon.progressionservice.exception.CustomContentServiceErrorDecoder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class FeignExceptionConfig {
     @Bean
     public ErrorDecoder contentServiceErrorDecoder(){
